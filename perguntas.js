248d61b3a3d120702e7d65135c54f4ca1c499edb
adriel1407 <00001101806448sp@al.educacao.sp.gov.br>
2024-11-28 10:25:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Marketing',
+    'Por que o marketing digital é importante?',
+    'Porque amplia o alcance e gera resultados com baixo custo.'
+)
+
+criaCartao(
+    'Design',
+    'O que diferencia design gráfico de design de produto?',
+    'O gráfico cria visuais; o de produto foca em objetos físicos.'
+)
+
+criaCartao(
+    'Gastronomia',
+    'Qual é o segredo de uma boa massa de pizza?',
+    'Ingredientes de qualidade e fermentação lenta.'
+)
+
+criaCartao(
+    'Treino',
+    'Por que aquecer antes de treinar?',
+    'Para prevenir lesões e melhorar o desempenho.'
+)
